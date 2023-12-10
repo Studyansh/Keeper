034612e7e10494a3221108da8834e012cbf21005
Studyansh <128509461+Studyansh@users.noreply.github.com>
2023-12-10 18:25:09 +0530

    added index.js, header.jsx, footer.jsx, styles.css
    Here , this files are to show the code as it wont work properly  because you have add json files and module files.

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return (
+        <div className="note">
+            <h1>This is note keeper</h1>
+            <p>Write your things to do here.</p>
+        </div>
+    );
+}
+
+export default Note;
